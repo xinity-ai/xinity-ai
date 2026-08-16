56980520c670518a9bcbc5bddcf6f4bb1ee3ffa1
Jonas <jv@xinity.ai>
2026-08-16 14:08:31 +0200

    feat(infoserver): add a gpu class table with bandwidth and compute figures

--- packages/xinity-infoserver/index.ts
+++ packages/xinity-infoserver/index.ts
@@ -2,6 +2,7 @@ export * from "./definitions/model-primitives";
 export * from "./definitions/licenses";
 export * from "./definitions/model-definition";
 export * from "./model-tags";
+export { classifyGpu, gpuClassPatterns, type GpuClass } from "./gpu-classes";
 export { satisfiesMinVersion, normalizePep440 } from "./semver";
 export { checkNodeCompatibility, isDeployableOnCluster, isLegacyModelDeployableOnCluster, explainClusterIncompatibility, modelRequirementsForDriver, requiredFeaturesForEngine, type GpuInfo, type NodeCapability, type ModelNodeRequirements, type IncompatibilityReason, type ClusterModel } from "./node-compat";
 export { createCatalogClient, createInfoserverClient, type CatalogClient, type InfoserverClient, type ModelLookup } from "./client";
